33ef97c3f012fc20225d1d4f293bb50f897ec68a
DmGrt <grytsishin@gmail.com>
2023-01-09 00:56:55 +0200

    Application and configuration

--- src/main/java/com/hrytsyshyn/eventhandler/EventHandlerApplication.java
+++ src/main/java/com/hrytsyshyn/eventhandler/EventHandlerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EventHandlerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EventHandlerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(EventHandlerApplication.class, args);
+  }
 }

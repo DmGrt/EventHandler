18cfdd1c328ab0a1f0a25c81f1e4509febf88e71
DmGrt <grytsishin@gmail.com>
2023-01-09 21:35:21 +0200

    Application and configuration

--- src/main/java/com/hrytsyshyn/eventhandler/service/time/TimeSeriesService.java
+++ src/main/java/com/hrytsyshyn/eventhandler/service/time/TimeSeriesService.java
@@ -1,16 +1,19 @@
 package com.hrytsyshyn.eventhandler.service.time;
 
 import com.hrytsyshyn.eventhandler.model.entity.Event;
-import com.hrytsyshyn.eventhandler.model.entity.EventParameter;
 import com.hrytsyshyn.eventhandler.service.common.EventService;
 import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.InfluxQLQueryApi;
 import com.influxdb.client.WriteApiBlocking;
+import com.influxdb.client.domain.InfluxQLQuery;
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
+import com.influxdb.query.InfluxQLQueryResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -25,9 +28,21 @@ public class TimeSeriesService {
       event
           .getEventParameters()
           .forEach(
-              eventParameter -> point.addField(eventParameter.getName(), eventParameter.getValue()));
+              eventParameter ->
+                  point.addField(eventParameter.getName(), eventParameter.getValue()));
       final WriteApiBlocking writeApiBlocking = influxDBClient.getWriteApiBlocking();
       writeApiBlocking.writePoint(point);
+    }
+  }
+
+  public InfluxQLQueryResult getStatistic(final String fieldName, final LocalDateTime fromTime) {
+    try (InfluxDBClient influxDBClient = influxDBConnection.buildConnection()) {
+      final InfluxQLQueryApi influxQLQueryApi = influxDBClient.getInfluxQLQueryApi();
+      final InfluxQLQuery query =
+          new InfluxQLQuery(
+              String.format("SELECT * FROM %s WHERE %s.date > %s", fieldName, fieldName, fromTime),
+              "db");
+      return influxQLQueryApi.query(query);
     }
   }
 }
